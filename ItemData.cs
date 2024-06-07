43d6df3fe6299e9372b92aef72c8aabf6ad5b1eb
William <michalkaliszuk7@gmail.com>
2024-06-07 12:51:38 +0200

    initial commit

---
+++ ItemData.cs
@@ -0,0 +1,10 @@
+using System.Data.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="Item Deta", menuName ="Item Data", order =50)]
+public class itemdata : ScriptableObject
+{
+public string itemName = "Item Name";
+public Sprite icon;
+}
