c47dd53d5cf97fb1ce23ff156d8125e9bf2509cc
William <michalkaliszuk7@gmail.com>
2024-06-07 13:27:56 +0200

    Initial commit

---
+++ GameResources.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResources : MonoBehaviour
+{
+private static GameResources instance;
+
+public static GameResources Instance {
+    get {
+        if(instance == null) {
+            instance = Resources.Load<GameResources>("GameResources");
+        }
+        return instance;
+    }
+}
+[Tooltip("populate with the dungeon roomnodetypelistSO")]
+public RoomNodeTypeList roomNodeTypeList;
+}
