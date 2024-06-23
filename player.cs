c692f016769cd7ae4a0cb28090bc279882c5ef85
William <michalkaliszuk7@gmail.com>
2024-06-24 00:30:47 +0200

    Started adding speed system

--- player.cs
+++ player.cs
@@ -27,6 +27,8 @@ public enum Dice {
         d20,
 }
 public Dice dice;
+public int speed =  1;
+
 void Start()
 {
     maxHealth = 100;
@@ -48,14 +50,14 @@ void OnTriggerEnter2D(Collider2D other)
     }
 }
 public void Movement() {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-        direction = Vector2.up;
-    } else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-        direction = Vector2.left;
-} else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-    direction = Vector2.down;
-} else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-    direction = Vector2.right;
+        if(Input.GetKey(KeyCode.W)) {
+        direction = Vector2.up * speed;
+    } else if(Input.GetKey(KeyCode.A)) {
+        direction = Vector2.left * speed;
+} else if(Input.GetKey(KeyCode.S)) {
+    direction = Vector2.down * speed;
+} else if(Input.GetKey(KeyCode.D)){
+    direction = Vector2.right * speed;
 } 
 if(Input.GetKeyUp(KeyCode.W) || (Input.GetKeyUp(KeyCode.UpArrow)) 
 || Input.GetKeyUp(KeyCode.A) || (Input.GetKeyUp(KeyCode.LeftArrow))
